b7145608f99a2660513f37624e32486ddcc39992
Ismail Sherif <ismailsherifwork@gmail.com>
2024-03-10 12:57:05 +0200

    Refactor code to improve performance and readability

---
+++ collections.py
@@ -0,0 +1,10 @@
+import collections.abc
+
+collections.MutableMapping = collections.abc.MutableMapping
+collections.Mapping = collections.abc.Mapping
+collections.MutableSequence = collections.abc.MutableSequence
+collections.Sequence = collections.abc.Sequence
+collections.Iterable = collections.abc.Iterable
+collections.Iterator = collections.abc.Iterator
+collections.MutableSet = collections.abc.MutableSet
+collections.Callable = collections.abc.Callable
