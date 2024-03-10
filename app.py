e51242f0681f2d8ecebf88794edc712e55ffca23
Ismail Sherif <ismailsherifwork@gmail.com>
2024-03-10 21:08:44 +0200

    Add model arguments and create Engine resource

--- app.py
+++ app.py
@@ -4,7 +4,22 @@ from flask_restful import Api, Resource, reqparse
 app = Flask(__name__)
 api = Api(app)
 
-class HelloWorld(Resource):
+model_args = reqparse.RequestParser()
+model_args.add_argument("headache", type=str, help="headache value is required", required=True)
+model_args.add_argument("back_pain", type=str, help="back_pain value is required", required=True)
+model_args.add_argument("chest_pain", type=str, help="chest_pain value is required", required=True)
+model_args.add_argument("cough", type=str, help="cough value is required", required=True)
+model_args.add_argument("fainting", type=str, help="fainting value is required", required=True)
+model_args.add_argument("sore_throat", type=str, help="sore_throat value is required", required=True)
+model_args.add_argument("fatigue", type=str, help="fatigue value is required", required=True)
+model_args.add_argument("restlessness", type=str, help="restlessness value is required", required=True)
+model_args.add_argument("low_body_temp", type=str, help="low_body_temp value is required", required=True)
+model_args.add_argument("fever", type=str, help="fever value is required", required=True)
+model_args.add_argument("sunken_eyes", type=str, help="sunken_eyes value is required", required=True)
+model_args.add_argument("nausea", type=str, help="nausea value is required", required=True)
+model_args.add_argument("blurred_vision", type=str, help="blurred_vision value is required", required=True)
+
+class Engine(Resource):
     def get(self):
         return {'hello': 'world'}
     
@@ -14,7 +29,7 @@ class HelloWorld(Resource):
         args = parser.parse_args()
         return {'name': args['name']}
     
-api.add_resource(HelloWorld, '/')
+api.add_resource(Engine, '/model')
 
 """
 request: symptoms = { {} }
