332bd8b082191a29e3d2c8525aca69fc60c885df
Ismail Sherif <ismailsherifwork@gmail.com>
2024-03-11 22:14:54 +0200

    Refactor app.py and test.py: building simple api

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask
 from flask_restful import Api, Resource, reqparse
-import packages
-from expert_system import ExpertSystem
-from controller import Controller
+# import packages
+# from expert_system import ExpertSystem
+# from controller import Controller
 
 app = Flask(__name__)
 api = Api(app)
@@ -23,17 +23,43 @@ model_args.add_argument("nausea", type=str, help="nausea value is required", req
 model_args.add_argument("blurred_vision", type=str, help="blurred_vision value is required", required=True)
 
 class Model(Resource):
-    def post(self):
+    def __init__(self):
+        self.base_data = {
+            "headache": True,
+            "back_pain": True,
+            "chest_pain": False,
+            "cough": True,
+            "fainting": True,
+            "sore_throat": False,
+            "fatigue": False,
+            "restlessness": True,
+            "low_body_temp": False,
+            "fever": True,
+            "sunken_eyes": False,
+            "nausea": False,
+            "blurred_vision": True
+        }
+
+    def Matched(self, raw_data):
+        for key in raw_data:
+            if raw_data[key] != self.base_data[key]:
+                return False
+        return True
+
+    def POST(self):
         # newCont = Controller()
         # newCont.preprocess()
         # engine = ExpertSystem(newCont.symptom_map, newCont.if_not_matched, newCont.get_treatments, newCont.get_details)
         # engine.reset()
         # engine.run()
         
-        args = model_args.parse_args()
-        return {'name': args['name']}
+        raw_data = model_args.parse_args()
+        if self.Matched(raw_data):
+            return {'response_status': 'Matched'}
+        else:
+            return {'response_status': 'Not Matched'}
     
-api.add_resource(Model, '/model')
+api.add_resource(Model, '/api')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- test.py
+++ test.py
@@ -1,16 +1,26 @@
+import requests
 
 data = {
-    "headache": "yes",
-    "back_pain": "yes",
-    "chest_pain": "yes",
-    "cough": "yes",
-    "fainting": "yes",
-    "sore_throat": "yes",
-    "fatigue": "yes",
-    "restlessness": "yes",
-    "low_body_temp": "yes",
-    "fever": "yes",
-    "sunken_eyes": "yes",
-    "nausea": "yes",
-    "blurred_vision": "yes"
+    "headache": True,
+    "back_pain": True,
+    "chest_pain": False,
+    "cough": True,
+    "fainting": True,
+    "sore_throat": False,
+    "fatigue": False,
+    "restlessness": True,
+    "low_body_temp": False,
+    "fever": True,
+    "sunken_eyes": False,
+    "nausea": False,
+    "blurred_vision": True
 }
+
+BASE = "http://127.0.0.1:5000/"
+
+def test():
+    return requests.POST(BASE + '/api', data)
+
+if __name__ == "__main__":
+    print("Getting response from server...\n", test())
+    print("Everything passed")
