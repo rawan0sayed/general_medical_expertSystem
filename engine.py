362a0e96f889c72bbcd30777610037e71d8b91de
Ismail Sherif <ismailsherifwork@gmail.com>
2024-03-12 15:51:12 +0200

    Refactor engine.py and update index.html

--- engine.py
+++ engine.py
@@ -28,39 +28,3 @@ class ExpertSystem:
 
     def run(self):
         return self.diagnose_disease()
-
-
-
-if __name__ == "__main__":
-    patient_data = {
-        "fever": True,
-        "cough": True,
-        "runny_nose": True,
-        "sore_throat": True,
-        "fatigue": True,
-        "body_aches": True,
-        "rash": True,
-        "itchiness": True,
-        "swollen_glands": True,
-        "vomiting": True,
-        "diarrhea": True,
-        "abdominal_pain": True,
-        "wheezing": True,
-        "difficulty_breathing": True,
-        "ear_pain": True,
-        "difficulty_sleeping": True,
-        "sneezing": True,
-        "itchy_eyes": True,
-        "high_fever": True,
-        "blisters": True,
-        "excessive_thirst": True,
-        "frequent_urination": True,
-        "unexplained_weight_loss": True,
-        "facial_pain": True,
-        "headache": True,
-        "nasal_congestion": True,
-        "dehydration": True
-    }
-
-    engine = ExpertSystem(patient_data)
-    result = engine.run()
