446c0af53949262b62f4913cc2f1e034fb9000d7
Ismail Sherif <ismailsherifwork@gmail.com>
2024-03-14 00:20:54 +0200

    Update user_data in api-test.py

--- api-test.py
+++ api-test.py
@@ -6,33 +6,30 @@ import json
 BASE = "https://medical-es-api.vercel.app"
 
 user_data = {
-    "fever": True,
-    "cough": True,
-    "runny_nose": True,
-    "sore_throat": False,
-    "fatigue": True,
-    "body_aches": True,
-    "rash": True,
-    "itchiness": True,
-    "swollen_glands": False,
-    "vomiting": False,
-    "diarrhea": True,
-    "abdominal_pain": True,
-    "wheezing": True,
-    "difficulty_breathing": True,
-    "ear_pain": False,
-    "difficulty_sleeping": True,
-    "sneezing": True,
-    "itchy_eyes": False,
-    "high_fever": False,
-    "blisters": True,
-    "excessive_thirst": True,
-    "frequent_urination": True,
-    "unexplained_weight_loss": True,
-    "facial_pain": False,
-    "headache": True,
-    "nasal_congestion": True,
-    "dehydration": False
+    "fever": 1,
+    "cough": 0,
+    "runny_nose": 0,
+    "sore_throat": 1,
+    "fatigue": 1,
+    "body_aches": 0,
+    "rash": 1,
+    "itchiness": 1,
+    "swollen_glands": 0,
+    "vomiting": 0,
+    "diarrhea": 1,
+    "abdominal_pain": 0,
+    "ear_pain": 0,
+    "sneezing": 1,
+    "itchy_eyes": 1,
+    "frequent_urination": 1,
+    "unexplained_weight_loss": 1,
+    "dehydration"
+    "difficulty_breathing": 0,
+    "difficulty_sleeping": 0,
+    "blisters": 0,
+    "excessive_thirst": 1,
+    "nasal_congestion": 1,
+    "headache": 0,
 }
 
 response = requests.post(
@@ -45,8 +42,8 @@ response = requests.post(
 )
 
 print( response.json() )
-print( type(response.json()) )
 
 # curl -X POST http://127.0.0.1:5000/api -H 'Content-Type: application/json' -d '{"name": "hello", "age": 22}'
 # curl -X POST https://medicales.vercel.app/api -H 'Content-Type: application/json' -d '{"name": "hello", "age": 22}'
 # curl -X POST https://medical-es-api.vercel.app/api -H 'Content-Type: application/json' -d '{ "fever": 1, "cough": 1, "runny_nose": 1, "sore_throat": 0, "fatigue": 1, "body_aches": 1, "rash": 1, "itchiness": 1, "swollen_glands": 0, "vomiting": 0, "diarrhea": 1, "abdominal_pain": 1, "wheezing": 1, "difficulty_breathing": 1, "ear_pain": 0, "difficulty_sleeping": 1, "sneezing": 1, "itchy_eyes": 0, "high_fever": 0, "blisters": 1, "excessive_thirst": 1, "frequent_urination": 1, "unexplained_weight_loss": 1, "facial_pain": 0, "headache": 1, "nasal_congestion": 1, "dehydration": 0 }'
+# curl -X POST https://medical-es-api.vercel.app/api -H 'Content-Type: application/json' -d '{ "fever": 1, "cough": 0, "runny_nose": 0, "sore_throat": 1, "fatigue": 1, "body_aches": 0, "rash": 1, "itchiness": 1, "swollen_glands": 0, "vomiting": 0, "diarrhea": 1, "abdominal_pain": 0, "ear_pain": 0, "sneezing": 1, "itchy_eyes": 1, "frequent_urination": 1, "unexplained_weight_loss": 1, "dehydration" "difficulty_breathing": 0, "difficulty_sleeping": 0, "blisters": 0, "excessive_thirst": 1, "nasal_congestion": 1, "headache": 0, }'
